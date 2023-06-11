f106cc8de09f242cfea0b535e7327229471ce939
Amey Parundekar <parundekaramey@gmail.com>
2023-06-11 18:00:46 -0400

    reverted changes

---
+++ assets/js/0a942827.aa66d8ae.js
@@ -0,0 +1,48 @@
+"use strict";(self.webpackChunkbaena=self.webpackChunkbaena||[]).push([[480],{511:(e,t,a)=>{a.r(t),a.d(t,{assets:()=>g,contentTitle:()=>p,default:()=>h,frontMatter:()=>s,metadata:()=>m,toc:()=>u});var n=a(7462),i=a(7294),o=a(3905);const r=a(9521).ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 1rem 2rem;
+  max-width: 250px;
+
+  img {
+    border-radius: 50%;
+    margin-top: 0rem;
+    object-fit: cover;
+    object-position: center;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: bold;
+    margin: 0;
+    margin-bottom: 0.5rem;
+    text-align: center;
+  }
+
+  p {
+    font-size: 1.2rem;
+    color: #666;
+    margin: 0;
+    text-align: center;
+  }
+
+  @media (min-width: 768px) {
+    flex-basis: calc(33.33% - 2rem);
+  }
+
+  @media (min-width: 1024px) {
+    flex-basis: calc(25% - 2rem);
+  }
+
+  .circle {
+    border-radius: 50%;
+    height: 150px;
+    width: 150px;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   }
+`,l=e=>{let{team:t}=e;return i.createElement("div",null,i.createElement("div",{style:{display:"flex",flexWrap:"wrap"}},t.map((e=>i.createElement(r,{key:e.id},i.createElement("a",{href:`${e.link}`},i.createElement("div",{className:"circle"},i.createElement("img",{src:e.photoUrl,alt:e.name})),i.createElement("h2",null,e.name),i.createElement("p",null,e.designation)))))))},d=JSON.parse('{"B":[{"id":1,"name":"Juan-Pablo Correa-Baena","designation":"Principal Investigator","photoUrl":"/img/people/prof.jpeg","link":"./prof"},{"id":2,"name":"Carlo Perini","designation":"Research Scientist","photoUrl":"/img/people/carlo.png","link":"./Research Scientists/carlo"},{"id":2,"name":"Kunal Datta","designation":"Postdoctoral Researcher","photoUrl":"/img/people/kunal.jpg","link":"./Post Doctoral Researchers/kunal"},{"id":4,"name":"Andres Felipe Castro Mendez","designation":"PhD Student","photoUrl":"/img/people/andres.jpg","link":"./Graduate Students/andres"},{"id":5,"name":"Diana LaFollette","designation":"PhD Student","photoUrl":"/img/people/diana.jpg","link":"./Graduate Students/diana"},{"id":6,"name":"Jingwei Yang","designation":"PhD Student","photoUrl":"/img/people/jingwei.png","link":"./Graduate Students/jingwei"},{"id":7,"name":"Juanita Hidalgo","designation":"PhD Student","photoUrl":"/img/people/juanita.png","link":"./Graduate Students/juanita"},{"id":8,"name":"Martin Gomez-Dominguez","designation":"PhD Student","photoUrl":"/img/people/martin.jpg","link":"./Graduate Students/martin"},{"id":9,"name":"Sanggyun Kim","designation":"PhD Student","photoUrl":"/img/people/sanggyun.jpg","link":"./Graduate Students/sanggyun"},{"id":10,"name":"Jack Lawton","designation":"PhD Student","photoUrl":"/img/people/jack.jpg","link":"./Graduate Students/jack"},{"id":11,"name":"Donghyun Oh","designation":"PhD Student","photoUrl":"/img/people/donghyun.jpg","link":"./Graduate Students/donghyun"},{"id":12,"name":"Sakshi Sharma","designation":"MS Student","photoUrl":"/img/people/sakshi.jpg","link":"./Graduate Students/sakshi"},{"id":13,"name":"Amey Parundekar","designation":"Graduate Assistant","photoUrl":"/img/people/amey.jpeg","link":"./Graduate Students/amey"}]}'),s={id:"team"},p="Our Team",m={unversionedId:"People/team",id:"People/team",title:"Our Team",description:"2022 Retreat",source:"@site/docs/02-People/00-team.mdx",sourceDirName:"02-People",slug:"/People/team",permalink:"/docs/People/team",draft:!1,tags:[],version:"current",sidebarPosition:0,frontMatter:{id:"team"},sidebar:"tutorialSidebar",previous:{title:"Vapor Processing: Evaporation, ALD, and VPI",permalink:"/docs/Research/vapor-processing"},next:{title:"Principal Investigator",permalink:"/docs/People/prof"}},g={},u=[],c={toc:u};function h(e){let{components:t,...i}=e;return(0,o.kt)("wrapper",(0,n.Z)({},c,i,{components:t,mdxType:"MDXLayout"}),(0,o.kt)("h1",{id:"our-team"},"Our Team"),(0,o.kt)("p",null,(0,o.kt)("img",{alt:"2022 Retreat",src:a(1185).Z,width:"2774",height:"1456"})),(0,o.kt)("br",null),(0,o.kt)(l,{team:d.B,mdxType:"TeamMembers"}))}h.isMDXComponent=!0},1185:(e,t,a)=>{a.d(t,{Z:()=>n});const n=a.p+"assets/images/group_photo-911b5fff935fab57e11e1a35c55e496d.jpg"}}]);
